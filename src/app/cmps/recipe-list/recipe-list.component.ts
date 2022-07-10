ee9aa620f5ee121bccb5e926eeaab03842a997d7
Erez <erezcomics@gmail.com>
2022-07-10 15:56:10 +0300

    commit 3 - added recipe & ingredient models

--- src/app/cmps/recipe-list/recipe-list.component.ts
+++ src/app/cmps/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe.model';
 
 @Component({
   selector: 'recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Hamburger', 'The Big-Fat burger.', 'https://www.thespruceeats.com/thmb/0GBQylFVXB8zizcHIHVrXlBdS2k=/5089x2863/smart/filters:no_upscale()/slow-cooker-hamburgers-recipe-for-kids-2098104-hero-01-3dd9bf2b2ca748358047f2ff4e73b64c.jpg'),
+    new Recipe('Schnitzel', 'A tasty schnitzel.', 'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG'),
+  ];
 
   constructor() { };
 
